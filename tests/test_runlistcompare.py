5096cf5b1bc7513ed6902ad3950479e53927a225
Bede Constantinides <1007616+bede@users.noreply.github.com>
2019-07-08 10:43:37 +0100

    Adds E. coli test. Was failing due to Ns in refseq

--- tests/test_runlistcompare.py
+++ tests/test_runlistcompare.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python
 # Run from pipeline root directory
 
+import os
+import shutil
+import hashlib
 import subprocess
 
-def test_all():
-	subprocess.check_output('python runListCompare.py tests/data/test.ini', shell=True)
+
+cwd = os.getcwd()
+
+def run(cmd, cwd=cwd):
+    return subprocess.run(cmd,
+                          cwd=cwd,
+                          shell=True,
+                          check=True,
+                          universal_newlines=True,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE)
+
+def test_all_cd():
+    if os.path.isdir('tests/output/cd'):
+        shutil.rmtree('tests/output/cd')
+    run('python runListCompare.py tests/data/cd/cd.ini')
+    with open('tests/output/cd/align_positions.txt', 'rb') as fh:
+        contents = fh.read()
+    assert hashlib.md5(contents).hexdigest() == '83da02b151226c443430c5c54c43ccea'
+
+
+def test_all_ec():
+    if os.path.isdir('tests/output/ec'):
+        shutil.rmtree('tests/output/ec')
+    run('python runListCompare.py tests/data/ec/ec.ini')
+    with open('tests/output/ec/align_positions.txt', 'rb') as fh:
+        contents = fh.read()
+    assert hashlib.md5(contents).hexdigest() == '811f7f54d6d5a5867bdeb718f1ac6e9a'
